3162fc1803e7d373b909fa4b0a9a15c5eeb007ae
Antonin81 <Antonin.pidet@gmail.com>
2024-01-04 13:56:23 +0100

    Début du bug hunt : formats autres que jpg, jpeg ou png ont été interdits. Visuel potentiellement à revoir

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -20,25 +20,35 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    const fileExtension = fileName.split('.')[1]
+    console.log(fileExtension)
+    const authorizedExtensions = ["jpg","jpeg","png"]
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    if(authorizedExtensions.includes(fileExtension)){
+      const formData = new FormData()
+      const email = JSON.parse(localStorage.getItem("user")).email
+      formData.append('file', file)
+      formData.append('email', email)
+
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({fileUrl, key}) => {
+          console.log(fileUrl)
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    } else {
+      this.document.querySelector(`input[data-testid="file"]`).value=""
+      alert("Le fichier doit être au format jpg, jpeg ou png")
+    }
+    
   }
   handleSubmit = e => {
     e.preventDefault()
